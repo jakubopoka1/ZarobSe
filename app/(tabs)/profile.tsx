3ca6adb962e558ec65f53df76bc6fe91d50ce426
Piotr Bednarski <bednarskipiotrpawel@gmail.com>
2026-05-11 17:46:39 +0200

    Dodanie wczesnych ekranów logowania/rejestracji i powitania

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Button, Text, View } from "react-native";
 
 export default function Screen() {
-	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-				backgroundColor: "#F8FAFC",
-			}}>
-			<Text>Ekran w budowie</Text>
-		</View>
-	);
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F8FAFC",
+      }}
+    >
+      <Text>Ekran w budowie</Text>
+
+      <View style={{ marginTop: 20, gap: 10, width: 160 }}>
+        <Button title="Login" onPress={() => router.push("/login")} />
+
+        <Button title="Register" onPress={() => router.push("/register")} />
+
+        <Button title="Welcome" onPress={() => router.push("/welcome")} />
+      </View>
+    </View>
+  );
 }
